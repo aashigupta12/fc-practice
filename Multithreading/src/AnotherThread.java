34be76a8fd4b45565c005b74570b474b51eef7c6
Aashi Gupta <aashi.gupta.in@freecharge.com>
2023-03-02 15:13:34 +0530

    nayifile!

--- Multithreading/src/AnotherThread.java
+++ Multithreading/src/AnotherThread.java
@@ -1,15 +1,7 @@
 public class AnotherThread extends Thread{
 
-    public void run(){
-        System.out.println(ThreadColor.ANSI_BLUE + "hello from " + currentThread().getName());
-
-        try{
-            Thread.sleep(3000);
-        } catch(InterruptedException exception){
-            System.out.println(ThreadColor.ANSI_BLUE + "another thread woke me up");
-        }
-
-        System.out.println(ThreadColor.ANSI_BLUE + "Three seconds have passed and I am awake");
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
-
 }
